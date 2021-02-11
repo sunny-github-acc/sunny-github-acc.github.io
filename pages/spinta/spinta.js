32422608e2a1e03fac88097a329ae6cdc54e07df
sunny <karolis.kazak@gmail.com>
2021-02-11 20:35:02 +0200

    links fonts https update

--- pages/spinta/spinta.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import { handleProducts as handlePageProducts } from "../../js/product_loader.js";
-    
-window.addEventListener("DOMContentLoaded", handlePageProducts);
-function handlePageProductss() {console.log("s")}
